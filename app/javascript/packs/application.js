469e7cf9ce13d8f4a9d150788d1b82de14145cb6
Tony Rowan <trowan812@gmail.com>
2021-04-04 13:35:13 +0100

    Install turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,6 @@
 import "./application.css";
 
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
-import Rails from "@rails/ujs";
-
-Rails.start();
+require("@rails/ujs").start();
+require("turbolinks").start();
 
 import "controllers";
